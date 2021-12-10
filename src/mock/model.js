2d0083c48c751a971fbe035d739f136516115385
Jacopo <jacopotrapani89@gmail.com>
2021-12-10 10:44:31 +0100

    delete src/mock directory

--- src/mock/model.js
+++
@@ -1,3 +0,0 @@
-
-
-export const modelwop = {"companies":{"operations":[{"name":"get","method":"GET","model":{"id":{"id":"id","type":"id","label":"id","resourceName":null,"resource":null,"validators":["required"],"errorMessages":["This field is required"]},"name":{"id":"name","type":"string","label":"name","resourceName":null,"resource":null,"validators":["required"],"errorMessages":["This field is required"]}},"operationType":"collection"}],"resourceName":"companies","title":"companies"}}
